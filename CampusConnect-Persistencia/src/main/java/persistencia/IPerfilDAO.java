84da3d7f18b1e6b40db143d3d354d8a9750adf21
JesusLares23 <jesus.lares233383@potros.itson.edu.mx>
2026-04-07 18:30:06 -0700

    Creación de interfaz IGenericoDAO
    Se eliminó el código de las interfaces actuales por optar el uso de una interfaz genérica que contendrá los métodos mínimos especificados para cada entidad del sistema.

--- CampusConnect-Persistencia/src/main/java/persistencia/IPerfilDAO.java
+++ CampusConnect-Persistencia/src/main/java/persistencia/IPerfilDAO.java
@@ -9,16 +9,8 @@ import java.util.List;
  *
  * @author Equipo 2 - "Azul"
  */
-public interface IPerfilDAO {
+public interface IPerfilDAO extends IGenericoDAO<Perfil, Long> {
     
-    boolean crear(Perfil pefil, EntityManager em);
     
-    Perfil buscarPorId(int idPerfil, EntityManager em);
-    
-    List<Perfil> listar(EntityManager em);
-    
-    boolean actualizar(Perfil perfil, EntityManager em);
-    
-    boolean eliminar(Long id, EntityManager em);
     
 }
