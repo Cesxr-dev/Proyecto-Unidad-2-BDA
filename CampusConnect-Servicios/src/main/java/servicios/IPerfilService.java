8dce61de369ac83bbfa711ecc54b3569798790f0
DavidTL001 <david.tirado233047@potros.itson.edu.mx>
2026-04-13 02:03:55 -0700

    Add autenticar method to IPerfilService interface

--- CampusConnect-Servicios/src/main/java/servicios/IPerfilService.java
+++ CampusConnect-Servicios/src/main/java/servicios/IPerfilService.java
@@ -2,12 +2,14 @@
 package servicios;
 
 import dominio.Perfil;
+import java.util.List;
 
 /**
  *
  * @author Equipo 2 - "Azul"
  */
 public interface IPerfilService extends IGenericoService<Perfil, Long> {
+       Perfil autenticar(String correo, String contrasena);
     
     
     
