1ae251d7fb1d4a0809ea2e47b62e030cde8cacff
JesusLares23 <jesus.lares233383@potros.itson.edu.mx>
2026-04-15 01:49:55 -0700

    Squashed commit of the following:
    commit feb1b828e7c8f6b9a39fb29a087d79aaedb4714c
    Merge: 2ff4498 c9b1363
    Author: DavidTL001 <david.tirado233047@potros.itson.edu.mx>
    Date:   Wed Apr 15 02:30:09 2026 -0600
    
        Merge pull request #33 from Cesxr-dev/David
    
        David- Creacion de pantallas para perfil y edicion perfil y sus funcionalidades
    
    commit c9b1363a5ab9378861ab460082fa81274d3697f7
    Author: DavidTL001 <david.tirado233047@potros.itson.edu.mx>
    Date:   Wed Apr 15 01:28:38 2026 -0700
    
        Update LikeDAO.java
    
    commit 3a28e33c35c2c3e9bde85c8244a9fe275ff1764a
    Author: DavidTL001 <david.tirado233047@potros.itson.edu.mx>
    Date:   Wed Apr 15 01:28:35 2026 -0700
    
        Update FrmBuscar.form
    
    commit 33622b27175dc4601d5c2d87ab44ee553692a2f7
    Author: DavidTL001 <david.tirado233047@potros.itson.edu.mx>
    Date:   Wed Apr 15 01:28:33 2026 -0700
    
        Update FrmBuscar.java
    
    commit 415f31c7d91ee60d70dfe29e8fb7bfd6d1c061bf
    Author: DavidTL001 <david.tirado233047@potros.itson.edu.mx>
    Date:   Wed Apr 15 01:28:30 2026 -0700
    
        Delete PerfilFrm.form
    
    commit 4ec466e93f57986cc152f4a6b3060388fe8b7a27
    Author: DavidTL001 <david.tirado233047@potros.itson.edu.mx>
    Date:   Wed Apr 15 01:28:28 2026 -0700
    
        Delete PerfilFrm.java
    
    commit fe6ec66f25c429eb2e6027a2a835384c02bde2fc
    Author: DavidTL001 <david.tirado233047@potros.itson.edu.mx>
    Date:   Wed Apr 15 01:28:26 2026 -0700
    
        Create PnlEditarPerfil.java
    
    commit 42d79ae16d448b7462fbc3897639107f83cc7cba
    Author: DavidTL001 <david.tirado233047@potros.itson.edu.mx>
    Date:   Wed Apr 15 01:28:24 2026 -0700
    
        Create PnlPerfil.java
    
    commit 3ac98c1e1f26e89f24da28357f1d29f004a762dc
    Author: DavidTL001 <david.tirado233047@potros.itson.edu.mx>
    Date:   Wed Apr 15 01:28:22 2026 -0700
    
        Update PerfilService.java
    
    commit 976173721a404fea01065eb71aa6abad4f1f17e2
    Author: DavidTL001 <david.tirado233047@potros.itson.edu.mx>
    Date:   Wed Apr 15 01:28:20 2026 -0700
    
        Update persistence.xml
    
    commit e50e4d2853629abb1aaf1f31c3796ca546b1d694
    Author: DavidTL001 <david.tirado233047@potros.itson.edu.mx>
    Date:   Wed Apr 15 01:28:18 2026 -0700
    
        Update campus-connect-1.0-SNAPSHOT.jar
    
    commit a516b8ba583d1da041590617b62552012fe8d00a
    Author: DavidTL001 <david.tirado233047@potros.itson.edu.mx>
    Date:   Wed Apr 15 01:28:12 2026 -0700
    
        Update inputFiles.lst
    
    commit b5b79a2ff311767450e36aacbc4e1a29557af21c
    Author: DavidTL001 <david.tirado233047@potros.itson.edu.mx>
    Date:   Wed Apr 15 01:28:10 2026 -0700
    
        Delete createdFiles.lst
    
    commit 46cf58afb912857545e4f2731542eda06b4eea47
    Author: DavidTL001 <david.tirado233047@potros.itson.edu.mx>
    Date:   Wed Apr 15 01:28:08 2026 -0700
    
        Delete inputFiles.lst

---
+++ CampusConnect-Presentacion/src/main/java/presentacion/perfil/PnlEditarPerfil.java
@@ -0,0 +1,426 @@
+package presentacion.perfil;
+
+import dominio.Carrera;
+import dominio.InfoAdicional;
+import dominio.Perfil;
+import dominio.Sesion;
+import dominio.TipoInfo;
+import presentacion.registroPanel.PnlFoto;
+import java.awt.Color;
+import java.awt.Component;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
+import javax.swing.BorderFactory;
+import javax.swing.Box;
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+import javax.swing.border.Border;
+
+/**
+ * @author Equipo 2 - "Azul"
+ */
+public class PnlEditarPerfil extends JPanel {
+
+    private JTextField txtNombre;
+    private com.toedter.calendar.JDateChooser dateChooser;
+    private JComboBox<Carrera> comboCarrera;
+    private PnlFoto pnlFoto;
+
+    private JScrollPane scrollGustos;
+    private JScrollPane scrollHobbies;
+    private JScrollPane scrollIntereses;
+
+    private JTextArea txtAreaGustos;
+    private JTextArea txtAreaHobbies;
+    private JTextArea txtAreaIntereses;
+
+    private JButton btnGuardar;
+    private JButton btnCancelar;
+
+    private Perfil perfilActual;
+
+    // Colores de borde
+    private final Border bordeError = BorderFactory.createLineBorder(Color.RED);
+    private final Border bordeOk = BorderFactory.createLineBorder(new Color(200, 200, 200));
+
+    // Interface para notificar cambios al frame principal
+    public interface OnAccionListener {
+
+        void onGuardar();
+
+        void onCancelar();
+    }
+    private OnAccionListener accionListener;
+
+    public PnlEditarPerfil(OnAccionListener listener) {
+        this.accionListener = listener;
+        inicializarComponentes();
+        construirLayout();
+        cargarDatosPerfil();
+    }
+
+    private void inicializarComponentes() {
+        pnlFoto = new PnlFoto();
+
+        txtNombre = new JTextField();
+        dateChooser = new com.toedter.calendar.JDateChooser();
+        comboCarrera = new JComboBox<>(Carrera.values());
+
+        txtAreaGustos = new JTextArea();
+        txtAreaHobbies = new JTextArea();
+        txtAreaIntereses = new JTextArea();
+
+        // Configurar áreas de texto
+        txtAreaGustos.setLineWrap(true);
+        txtAreaGustos.setWrapStyleWord(true);
+        txtAreaHobbies.setLineWrap(true);
+        txtAreaHobbies.setWrapStyleWord(true);
+        txtAreaIntereses.setLineWrap(true);
+        txtAreaIntereses.setWrapStyleWord(true);
+
+        // Envolver en JScrollPane
+        scrollGustos = new JScrollPane(txtAreaGustos);
+        scrollHobbies = new JScrollPane(txtAreaHobbies);
+        scrollIntereses = new JScrollPane(txtAreaIntereses);
+
+        Dimension tamanoArea = new Dimension(Integer.MAX_VALUE, 80);
+        scrollGustos.setMaximumSize(tamanoArea);
+        scrollHobbies.setMaximumSize(tamanoArea);
+        scrollIntereses.setMaximumSize(tamanoArea);
+
+        scrollGustos.setAlignmentX(Component.LEFT_ALIGNMENT);
+        scrollHobbies.setAlignmentX(Component.LEFT_ALIGNMENT);
+        scrollIntereses.setAlignmentX(Component.LEFT_ALIGNMENT);
+
+        // Configurar campos de texto
+        txtNombre.setMaximumSize(new Dimension(Integer.MAX_VALUE, 30));
+        dateChooser.setMaximumSize(new Dimension(Integer.MAX_VALUE, 30));
+        comboCarrera.setMaximumSize(new Dimension(Integer.MAX_VALUE, 30));
+
+        txtNombre.setAlignmentX(Component.LEFT_ALIGNMENT);
+        dateChooser.setAlignmentX(Component.LEFT_ALIGNMENT);
+        comboCarrera.setAlignmentX(Component.LEFT_ALIGNMENT);
+
+        // Botones
+        btnGuardar = new JButton("GUARDAR CAMBIOS");
+        btnGuardar.setBackground(new Color(34, 139, 34));
+        btnGuardar.setForeground(Color.WHITE);
+        btnGuardar.setFont(new Font("SansSerif", Font.BOLD, 12));
+        btnGuardar.setMaximumSize(new Dimension(Integer.MAX_VALUE, 40));
+        btnGuardar.setAlignmentX(Component.CENTER_ALIGNMENT);
+        btnGuardar.addActionListener(e -> guardarCambios());
+
+        btnCancelar = new JButton("CANCELAR");
+        btnCancelar.setBackground(new Color(169, 169, 169));
+        btnCancelar.setForeground(Color.WHITE);
+        btnCancelar.setFont(new Font("SansSerif", Font.BOLD, 12));
+        btnCancelar.setMaximumSize(new Dimension(Integer.MAX_VALUE, 40));
+        btnCancelar.setAlignmentX(Component.CENTER_ALIGNMENT);
+        btnCancelar.addActionListener(e -> {
+            if (accionListener != null) {
+                accionListener.onCancelar();
+            }
+        });
+    }
+
+    private void construirLayout() {
+        setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+        setBorder(BorderFactory.createEmptyBorder(15, 15, 15, 15));
+        setBackground(Color.WHITE);
+
+        // ===== FOTO =====
+        JPanel pnlFotoContenedor = new JPanel();
+        pnlFotoContenedor.setLayout(new BoxLayout(pnlFotoContenedor, BoxLayout.X_AXIS));
+        pnlFotoContenedor.setBackground(Color.WHITE);
+        pnlFotoContenedor.setMaximumSize(new Dimension(Integer.MAX_VALUE, 140));
+        pnlFotoContenedor.setAlignmentX(Component.CENTER_ALIGNMENT);
+        pnlFotoContenedor.add(Box.createHorizontalGlue());
+        pnlFoto.setAlignmentX(Component.CENTER_ALIGNMENT);
+        pnlFotoContenedor.add(pnlFoto);
+        pnlFotoContenedor.add(Box.createHorizontalGlue());
+        add(pnlFotoContenedor);
+        add(Box.createVerticalStrut(15));
+
+        JLabel lblNombre = new JLabel("Nombre:");
+        lblNombre.setAlignmentX(Component.LEFT_ALIGNMENT);
+        add(lblNombre);
+        add(Box.createVerticalStrut(5));
+        add(txtNombre);
+        add(Box.createVerticalStrut(15));
+
+        JLabel lblFecha = new JLabel("Fecha de Nacimiento:");
+        lblFecha.setAlignmentX(Component.LEFT_ALIGNMENT);
+        add(lblFecha);
+        add(Box.createVerticalStrut(5));
+        add(dateChooser);
+        add(Box.createVerticalStrut(15));
+
+        JLabel lblCarrera = new JLabel("Carrera:");
+        lblCarrera.setAlignmentX(Component.LEFT_ALIGNMENT);
+        add(lblCarrera);
+        add(Box.createVerticalStrut(5));
+        add(comboCarrera);
+        add(Box.createVerticalStrut(15));
+
+        JLabel lblGustos = new JLabel("Gustos:");
+        lblGustos.setAlignmentX(Component.LEFT_ALIGNMENT);
+        add(lblGustos);
+        add(Box.createVerticalStrut(5));
+        add(scrollGustos);
+        add(Box.createVerticalStrut(15));
+
+        JLabel lblHobbies = new JLabel("Hobbies:");
+        lblHobbies.setAlignmentX(Component.LEFT_ALIGNMENT);
+        add(lblHobbies);
+        add(Box.createVerticalStrut(5));
+        add(scrollHobbies);
+        add(Box.createVerticalStrut(15));
+
+        JLabel lblIntereses = new JLabel("Intereses:");
+        lblIntereses.setAlignmentX(Component.LEFT_ALIGNMENT);
+        add(lblIntereses);
+        add(Box.createVerticalStrut(5));
+        add(scrollIntereses);
+        add(Box.createVerticalStrut(20));
+
+        add(btnGuardar);
+        add(Box.createVerticalStrut(10));
+        add(btnCancelar);
+        add(Box.createVerticalGlue());
+    }
+
+    /**
+     * Carga los datos actuales del perfil en los campos
+     */
+    private void cargarDatosPerfil() {
+        perfilActual = Sesion.getPerfilActivo();
+
+        if (perfilActual == null) {
+            return;
+        }
+
+        // Nombre
+        txtNombre.setText(perfilActual.getNombre());
+
+        // Fecha de nacimiento
+        LocalDate fecha = perfilActual.getFechaNacimiento();
+        if (fecha != null) {
+            Date date = java.sql.Date.valueOf(fecha);
+            dateChooser.setDate(date);
+        }
+
+        // Carrera
+        comboCarrera.setSelectedItem(perfilActual.getCarrera());
+
+        // Foto
+        String rutaFoto = perfilActual.getFotoPerfil();
+        if (rutaFoto != null && !rutaFoto.isEmpty()) {
+            pnlFoto.setImagen(rutaFoto);
+        }
+
+        // Separar gustos, hobbies e intereses
+        String gustos = "";
+        String hobbies = "";
+        String intereses = "";
+
+        if (perfilActual.getPerfilInfoAdicional() != null) {
+            for (InfoAdicional info : perfilActual.getPerfilInfoAdicional()) {
+                if (info.getTipo() == TipoInfo.GUSTO) {
+                    gustos = info.getNombre();
+                } else if (info.getTipo() == TipoInfo.HOBBIE) {
+                    hobbies = info.getNombre();
+                } else if (info.getTipo() == TipoInfo.INTERES) {
+                    intereses = info.getNombre();
+                }
+            }
+        }
+
+        txtAreaGustos.setText(gustos);
+        txtAreaHobbies.setText(hobbies);
+        txtAreaIntereses.setText(intereses);
+    }
+
+    /**
+     * Valida los campos del formulario
+     */
+    private boolean validarCampos() {
+        boolean valido = true;
+
+        // Validar nombre
+        if (txtNombre.getText().trim().isEmpty()) {
+            txtNombre.setBorder(bordeError);
+            valido = false;
+        } else {
+            txtNombre.setBorder(bordeOk);
+        }
+
+        // Validar fecha
+        if (dateChooser.getDate() == null) {
+            dateChooser.setBorder(bordeError);
+            valido = false;
+        } else {
+            LocalDate fecha = dateChooser.getDate().toInstant()
+                    .atZone(ZoneId.systemDefault()).toLocalDate();
+            if (fecha.isAfter(LocalDate.now().minusYears(18))) {
+                dateChooser.setBorder(bordeError);
+                valido = false;
+            } else {
+                dateChooser.setBorder(bordeOk);
+            }
+        }
+
+        // Validar gustos, hobbies, intereses
+        if (txtAreaGustos.getText().trim().isEmpty()) {
+            scrollGustos.setBorder(bordeError);
+            valido = false;
+        } else {
+            scrollGustos.setBorder(bordeOk);
+        }
+
+        if (txtAreaHobbies.getText().trim().isEmpty()) {
+            scrollHobbies.setBorder(bordeError);
+            valido = false;
+        } else {
+            scrollHobbies.setBorder(bordeOk);
+        }
+
+        if (txtAreaIntereses.getText().trim().isEmpty()) {
+            scrollIntereses.setBorder(bordeError);
+            valido = false;
+        } else {
+            scrollIntereses.setBorder(bordeOk);
+        }
+
+        return valido;
+    }
+
+    /**
+     * Obtiene los datos editados
+     */
+    private Perfil obtenerDatosEditados() {
+        Perfil perfil = new Perfil();
+        perfil.setId(perfilActual.getId());
+
+        // Nombre
+        perfil.setNombre(txtNombre.getText());
+
+        // Fecha
+        Date date = dateChooser.getDate();
+        LocalDate fecha = date.toInstant()
+                .atZone(ZoneId.systemDefault())
+                .toLocalDate();
+        perfil.setFechaNacimiento(fecha);
+
+        // Carrera
+        Carrera carrera = (Carrera) comboCarrera.getSelectedItem();
+        perfil.setCarrera(carrera);
+
+        // Foto (si se seleccionó una nueva)
+        String rutaFoto = pnlFoto.getRutaImagen();
+        if (rutaFoto != null && !rutaFoto.isEmpty()) {
+            perfil.setFotoPerfil(rutaFoto);
+        } else {
+            perfil.setFotoPerfil(perfilActual.getFotoPerfil());
+        }
+
+        // Mantener correo y contraseña del perfil actual
+        perfil.setCorreoInstitucional(perfilActual.getCorreoInstitucional());
+        perfil.setContrasena(perfilActual.getContrasena());
+
+        // Gustos, hobbies, intereses
+        Set<InfoAdicional> infoAdicionalSet = new HashSet<>();
+
+        InfoAdicional infoGustos = new InfoAdicional();
+        infoGustos.setTipo(TipoInfo.GUSTO);
+        infoGustos.setNombre(txtAreaGustos.getText());
+        infoAdicionalSet.add(infoGustos);
+
+        InfoAdicional infoHobbies = new InfoAdicional();
+        infoHobbies.setTipo(TipoInfo.HOBBIE);
+        infoHobbies.setNombre(txtAreaHobbies.getText());
+        infoAdicionalSet.add(infoHobbies);
+
+        InfoAdicional infoIntereses = new InfoAdicional();
+        infoIntereses.setTipo(TipoInfo.INTERES);
+        infoIntereses.setNombre(txtAreaIntereses.getText());
+        infoAdicionalSet.add(infoIntereses);
+
+        for (InfoAdicional info : infoAdicionalSet) {
+            info.getPerfiles().add(perfil);
+        }
+
+        perfil.setPerfilInfoAdicional(infoAdicionalSet);
+
+        return perfil;
+    }
+
+    /**
+     * Guarda los cambios realizados
+     */
+    private void guardarCambios() {
+        if (!validarCampos()) {
+            javax.swing.JOptionPane.showMessageDialog(this,
+                    "Por favor completa todos los campos correctamente",
+                    "Error de Validación",
+                    javax.swing.JOptionPane.WARNING_MESSAGE);
+            return;
+        }
+
+        // Mostrar confirmación
+        int respuesta = javax.swing.JOptionPane.showConfirmDialog(this,
+                "¿Estás seguro de que deseas guardar los cambios?",
+                "Confirmar cambios",
+                javax.swing.JOptionPane.YES_NO_OPTION,
+                javax.swing.JOptionPane.QUESTION_MESSAGE);
+
+        if (respuesta == javax.swing.JOptionPane.YES_OPTION) {
+            try {
+                Perfil perfilEditado = obtenerDatosEditados();
+
+                // Actualizar en BD
+                servicios.PerfilService perfilService = new servicios.PerfilService();
+                perfilService.actualizar(perfilEditado);
+
+                // Actualizar en sesión
+                Sesion.setPerfilActivo(perfilEditado);
+
+                javax.swing.JOptionPane.showMessageDialog(this,
+                        "¡Cambios guardados exitosamente!",
+                        "Éxito",
+                        javax.swing.JOptionPane.INFORMATION_MESSAGE);
+
+                // Notificar al listener
+                if (accionListener != null) {
+                    accionListener.onGuardar();
+                }
+            } catch (Exception e) {
+                javax.swing.JOptionPane.showMessageDialog(this,
+                        "Error al guardar cambios: " + e.getMessage(),
+                        "Error del Sistema",
+                        javax.swing.JOptionPane.ERROR_MESSAGE);
+            }
+        }
+    }
+
+    /**
+     * Limpia los bordes de error
+     */
+    private void limpiarBordes() {
+        txtNombre.setBorder(bordeOk);
+        dateChooser.setBorder(bordeOk);
+        scrollGustos.setBorder(bordeOk);
+        scrollHobbies.setBorder(bordeOk);
+        scrollIntereses.setBorder(bordeOk);
+    }
+}
